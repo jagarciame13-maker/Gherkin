e9cfd0269fbfb8f177ac7d3db07f5f40e2539117
jagarciame13-maker <jagarciame13@gmail.com>
2025-12-01 18:34:36 -0600

    Update test_votar.py

--- tests/test_votar.py
+++ tests/test_votar.py
@@ -1,19 +1,22 @@
 from pytest_bdd import scenarios, given, when, then, parsers
 
-
 scenarios("votar.feature")
 
 def puede_votar(edad):
     return "Sí puede votar" if edad >= 18 else "No puede votar"
+
 @given(parsers.parse("que la persona tiene {edad:d} años"), target_fixture="contexto")
 def given_edad(edad):
     return {"edad": edad}
+
 @when("se verifica si puede votar")
 def when_verifica(contexto):
     resultado = puede_votar(contexto["edad"])
     contexto["resultado"] = resultado
     return contexto
+
 @then(parsers.parse('el resultado debe ser "{texto}"'))
 def then_resultado(contexto, texto):
     assert contexto["resultado"] == texto
+
 
